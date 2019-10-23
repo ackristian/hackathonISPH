8bd6e987627fb0b1520f1d055486a7b6e947a241
José João <acbwesimples@gmail.com>
2019-10-23 10:08:39 +0100

    Primeiro commit Hackaton ISPH

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+	private Long codigo;
+	private String nome;
+	private BigDecimal preco;
+	private Integer quantidade;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
